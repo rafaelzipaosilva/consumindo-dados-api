39a9e22790f284dec65bc4136549e8ffc2b62786
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-01-06 22:26:16 -0300

    aula1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log('Mandando ou pro amigo');
+
+function mandaMensagem() {
+    console.log('Tudo bem?');
+    console.log('Vou tem mandar uma solicitação');
+    console.log('Solicitação recebida!');
+}
+
+setTimeout(mandaMensagem, 5000);
+
+console.log('Tchau tchau!')
